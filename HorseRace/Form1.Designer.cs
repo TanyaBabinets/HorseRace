7fa486acb2f882dd5a64e867ee5fbb038cca412d
Tanyababinets <tanya060616@gmail.com>
2023-09-04 15:21:20 +0300

    Add project files.

---
+++ HorseRace/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace HorseRace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.HORSE = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PLACE = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(120, 88);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(310, 46);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(120, 165);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(310, 46);
+            this.progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(120, 231);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(310, 46);
+            this.progressBar3.TabIndex = 2;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(120, 295);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(310, 46);
+            this.progressBar4.TabIndex = 3;
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(120, 361);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(310, 46);
+            this.progressBar5.TabIndex = 4;
+            this.progressBar5.Click += new System.EventHandler(this.progressBar5_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSalmon;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(120, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(310, 60);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Horse 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Horse 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Horse 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Horse 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 377);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Horse 5";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.HORSE,
+            this.PLACE});
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView1.Location = new System.Drawing.Point(500, 88);
+            this.listView1.Name = "listView1";
+            this.listView1.Scrollable = false;
+            this.listView1.Size = new System.Drawing.Size(257, 319);
+            this.listView1.TabIndex = 11;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.Salmon;
+            this.label6.Location = new System.Drawing.Point(564, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 32);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "RESULTS";
+            // 
+            // HORSE
+            // 
+            this.HORSE.Text = "HORSE";
+            this.HORSE.Width = 100;
+            // 
+            // PLACE
+            // 
+            this.PLACE.Text = "PLACE";
+            this.PLACE.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ColumnHeader HORSE;
+        private System.Windows.Forms.ColumnHeader PLACE;
+    }
+}
+
---
+++ HorseRace/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using ProgressBar = System.Windows.Forms.ProgressBar;
+
+namespace HorseRace
+{
+    public partial class Form1 : Form
+    {
+
+        public SynchronizationContext uiContext;
+
+        List<ProgressBar> pb = null;
+     //   List<Label> names = null;
+        Random rand = new Random();
+        int counter = 1;//когда лошадь добегает то в лист бокс записывается ее номер 
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Horse Racing";
+            uiContext = SynchronizationContext.Current;
+            pb = new List<ProgressBar> { progressBar1, progressBar2, progressBar3, progressBar4, progressBar5 };
+          //  names=new List<Label>(){label1,label2, label3, label4, label5};
+            progressBar1.Name = "Horse 1";
+            progressBar2.Name = "Horse 2";
+            progressBar3.Name = "Horse 3";
+            progressBar4.Name = "Horse 4";
+            progressBar5.Name = "Horse 5";
+
+        }
+        public void ThreadFunk(object obj)
+        {
+            ProgressBar pb2 = (ProgressBar)obj;
+
+
+            uiContext.Send(d => pb2.Minimum = 0, null);
+            uiContext.Send(d => pb2.Maximum = 230, null);
+            uiContext.Send(d => pb2.Value = 0, null);
+
+            int count = 0; //  значение прогресс Бара        
+                while (true)
+                {
+                    count += rand.Next(1, 15);
+                    if (count <= pb2.Maximum)
+                        uiContext.Send(d => pb2.Value = count, null);
+                    else
+                    {
+                    ListViewItem item = new ListViewItem(pb2.Name);
+                    item.SubItems.Add(counter.ToString());
+                      uiContext.Send(d => pb2.Value = count - (count - pb2.Maximum), null);
+                        uiContext.Send(d => listView1.Items.Add(item), null);
+                        counter++;
+                        break;
+                    }
+                    Thread.Sleep(100);
+
+
+                }
+            }
+
+        
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //foreach (var t in pb)
+            //    t.Abort();
+        }
+        
+        private void progressBar5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)///START
+        {
+            button1.Enabled = false;
+
+            foreach (var t in pb)// Создание и запуск потоков для каждого ProgressBar
+            {
+                Thread thread = new Thread(new ParameterizedThreadStart(ThreadFunk));
+                thread.IsBackground = true;
+                thread.Start(t);
+
+                Thread.Sleep(100);
+            }
+
+        }
+    }
+}
+
+
+
